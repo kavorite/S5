4a1e384b2779573be199e7dd29816ce4a3eaddfb
kavorite <kavorite@gmail.com>
2022-11-17 12:14:45 -0500

    migrate to poetry

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_namespace_packages, setup
-
-setup(
-    name="s5",
-    version="0.0.1",
-    description="s5 in dm-haiku",
-    install_requires=["dm_haiku", "jax", "numpy", "scipy"],
-    package=find_namespace_packages("_src"),
-)
